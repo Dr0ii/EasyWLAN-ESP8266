a1e28c253602e3f4f93ecc30dd52fea462c67cba
DREII <jockien@qq.com>
2022-10-12 22:54:12 +0800

    文件上传

---
+++ EsayWLAN/init.h
@@ -0,0 +1,59 @@
+
+#ifndef INIT_H
+#define INIT_H
+
+#include <ESP8266WiFi.h>
+#include <DNSServer.h>
+#include "serve.h"
+
+//AP（服务端）
+#define AP_SSID "动凡科技™"   //名称
+#define AP_PASSWORD ""       //密码，空为不设置密码
+#define AP_MAX_CLIENT 1      //可接入数，最多4
+IPAddress IP(2,2,2,2);       //服务地址 （英文逗号隔开）
+bool DNS = true;             //是否启动DNS服务（接入热点自动弹出配网界面）
+DNSServer dnsServe;
+
+void wifi_scan_ssid(int scanNum){
+  ScanResult = "[";
+  String type;
+  String hidden;
+  Serial.println("");
+  Serial.print("正在扫描附近WIFI热点：");
+  for(int i = 0; i < scanNum; i++){
+    WiFi.encryptionType(i) == ENC_TYPE_NONE ? type = "1" : type = "0";
+    WiFi.isHidden(i) ? hidden = "0" : hidden = "1";
+    ScanResult += "{\"ssid\":\"" + WiFi.SSID(i) + "\"," + "\"dbm\":\"" + WiFi.RSSI(i) + "\"," + "\"type\":\"" + type + "\"," +  "\"hidden\":\"" + hidden + "\"},";
+    Serial.print(".");
+  }
+  Serial.println("");
+  ScanResult += "{\"end\":\"true\"}]";
+  Serial.print(ScanResult);
+  Serial.println("");
+  Serial.println("完成扫描");
+  wifi_serve_init();
+}
+
+void wifi_init(){
+	WiFi.persistent(false);
+  WiFi.mode(WIFI_AP_STA);
+  WiFi.softAPConfig(IP, IP, IPAddress(255,255,255,0));
+  WiFi.softAP(AP_SSID, AP_PASSWORD, 1, 0, AP_MAX_CLIENT);
+  delay(300);
+  Serial.println("");
+  Serial.print("服务已启动，IP地址为： ");
+  Serial.print(WiFi.softAPIP());
+  Serial.println("");
+  WiFi.scanNetworksAsync(wifi_scan_ssid);
+}
+
+void wifi_dns_init(){
+  if(DNS){
+    dnsServe.start((byte)53, "*", IP);
+    Serial.println("");
+    Serial.println("DNS服务已启动");
+  };
+  return;
+}
+
+#endif
---
+++ EsayWLAN/serve.h
@@ -0,0 +1,95 @@
+
+#ifndef SERVE_H
+#define SERVE_H
+
+#include <ESP8266WebServer.h>
+
+#include "init.h"
+#include "index.h"
+
+ESP8266WebServer serve(80);
+
+// STA（客户端）
+String STA_SSID;      //WiFi名称
+String STA_PASSWORD;  //WiFi密码
+String ScanResult;
+
+void wifi_scan_again_ssid(int scanNum){
+  ScanResult = "[";
+  String type;
+  String hidden;
+  Serial.println("");
+  Serial.println("");
+  Serial.print("正在扫描附近WIFI热点：");
+  for(int i = 0; i < scanNum; i++){
+    WiFi.encryptionType(i) == ENC_TYPE_NONE ? type = "1" : type = "0";
+    WiFi.isHidden(i) ? hidden = "0" : hidden = "1";
+    ScanResult += "{\"ssid\":\"" + WiFi.SSID(i) + "\"," + "\"dbm\":\"" + WiFi.RSSI(i) + "\"," + "\"type\":\"" + type + "\"," +  "\"hidden\":\"" + hidden + "\"},";
+    Serial.print(".");
+  }
+  Serial.println("");
+  ScanResult += "{\"end\":true}]";
+  Serial.print(ScanResult);
+  Serial.println("");
+  Serial.println("完成扫描");
+  serve.send(200, "text/plain", ScanResult);
+  return;
+}
+
+void wifi_connecting(){
+  int i = 0;
+  Serial.println("接入热点中：");
+  WiFi.begin(STA_SSID, STA_PASSWORD);
+  for(size_t i = 0; i < 12; i++){
+    if(WiFi.status() == WL_CONNECTED){
+      Serial.println("");
+      Serial.println("热点连接成功");
+      serve.send(200, "text/plain", "{\"status\":true}");
+      return;
+    }else{
+      delay(500);
+      Serial.print(".");
+    }
+  }
+  Serial.println("");
+  Serial.println("热点连接超时");
+  serve.send(200, "text/plain", "{\"status\":false}");
+  return;
+}
+
+void www_configPage(){
+  serve.send(200, "text/html", configPage);
+  return;
+}
+
+void www_getData(){
+  serve.send(200, "text/plain", ScanResult);
+  return;
+}
+
+void www_getNewData(){
+  wifi_scan_again_ssid(WiFi.scanNetworks());
+}
+
+void www_handleData(){
+  Serial.println("");
+  Serial.print("SSID: ");
+  Serial.print(serve.arg("ssid"));
+  Serial.println("");
+  Serial.print("密码: ");
+  Serial.println(serve.arg("pwd"));
+  STA_SSID = serve.arg("ssid");
+  STA_PASSWORD = serve.arg("pwd");
+  wifi_connecting();
+}
+
+void wifi_serve_init(){
+  serve.begin();
+  serve.on("/", www_configPage);
+  serve.on("/getData", www_getData);
+  serve.on("/getNewData", www_getNewData);
+  serve.on("/ssidData", www_handleData);
+  serve.onNotFound(www_configPage);
+}
+
+#endif
